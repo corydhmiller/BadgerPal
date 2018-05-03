1d66644cf8938b280db7a50eb42f5675e27ad01b
Cory Miller <corydhmiller@gmail.com>
2018-05-03 06:52:55 +0100

    Construct login page

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ export default {
 </script>
 
 <style>
+body, html {
+  font-size: 18px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,23 +2,46 @@
   <div class="login">
     <h3>Sign In</h3>
     <input type="text" placeholder="Email" />
+    <br />
     <input type="password" placeholder="Password" />
+    <br />
     <button>Login</button>
     <p>Don't have an account yet? Create one</p>
   </div>
 </template>
 
 <script>
-  export default{
+  export default {
     name: 'login',
-    data: function() {
-      return {}
-    },
-    methods: {}
+    data () {
+      return {
+      }
+    }
   }
 
 </script>
 
 <style scoped>
+.login {
+  text-align:center;
+}
+input {
+  padding: 1rem;
+  margin: 1rem;
+  width:20%;
+  font-size: 1rem;
+}
+
+button {
+  width:20%;
+  background: #8AC396;
+  color:#f9f9f9;
+  padding: 1rem;
+  margin: 1rem;
+  font-weight:600;
+  border: none;
+  border-radius: .2rem;
+  font-size: 1rem;
+}
 
 </style>
