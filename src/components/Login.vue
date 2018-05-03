05de7cbd50b80f39f22cd2cfc07bf5edd72b8c53
Cory Miller <corydhmiller@gmail.com>
2018-05-03 06:43:51 +0100

    init commit

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email" />
+    <input type="password" placeholder="Password" />
+    <button>Login</button>
+    <p>Don't have an account yet? Create one</p>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'login',
+    data: function() {
+      return {}
+    },
+    methods: {}
+  }
+
+</script>
+
+<style scoped>
+
+</style>
