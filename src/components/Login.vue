c4a6dc03c7d59522046a4749760ff4fe313ae4bd
Cory Miller <corydhmiller@gmail.com>
2018-05-07 08:54:03 +0100

    Stylize inputs

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="login">
-    <h3>Sign In</h3>
-    <input type="text" v-model="email" placeholder="Email" />
-    <br />
-    <input type="password" v-model="password" placeholder="Password" />
-    <br />
-    <button v-on:click="submitLogin">Login &raquo;</button>
-    <div class="message">{{message}}</div>
-    <p>Don't have an account yet? <router-link to="/register">Create an account</router-link></p>
+    <div class="flex flex--center">
+      <h3>Sign In</h3>
+      <div class="styled-input">
+        <input type="email" v-model="email" required />
+        <label>Email</label>
+        <span></span>
+      </div>
+      <div class="styled-input">
+        <input type="password" v-model="password" required />
+        <label>Password</label>
+        <span></span>
+      </div>
+      <button class="button button--green button--shadow" v-on:click="submitLogin">Login &raquo;</button>
+      <div class="message">{{message}}</div>
+      <p>Don't have an account yet?
+        <router-link to="/register">Create an account</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -16,7 +26,7 @@
 
   export default {
     name: 'login',
-    data () {
+    data() {
       return {
         email: '',
         password: '',
@@ -30,13 +40,13 @@
       },
       submitLogin() {
         Firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(() => {
-          this.setMessageContent('You have successfully logged in.')
-          this.$store.dispatch('setUser');
-          this.$router.replace('app')
-        },
-        (err) => {
-          this.setMessageContent('Oops! ' + err.message)
-        }).catch((error) => {
+            this.setMessageContent('You have successfully logged in.')
+            this.$store.dispatch('setUser');
+            this.$router.replace('app')
+          },
+          (err) => {
+            this.setMessageContent('Oops! ' + err.message)
+          }).catch((error) => {
           this.setMessageContent(`An error occured during the login process.`)
         })
       }
@@ -47,8 +57,5 @@
 
 <style lang="scss" scoped>
 
-.login {
-  text-align:center;
-}
 
 </style>
